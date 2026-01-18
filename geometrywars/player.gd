36ae81d38688f27aeab60feda15e55c9489bb26a
Sytsn <jwclaus12@gmail.com>
2026-01-18 13:18:59 -0500

    moving box that looks at mouse

---
+++ geometrywars/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+@export var speed = 400
+
+
+func _physics_process(delta: float) -> void:
+	get_input()
+	move_and_slide()
+
+
+func get_input():
+	look_at(get_global_mouse_position())
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
